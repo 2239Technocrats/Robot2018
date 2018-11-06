85df3b4deeb909dd6ca465b46caa64d427c626a1
Hopkins Technocrats <frcteam2239@gmail.com>
2018-11-06 17:28:15 -0600

    Update work on ContrScheme.java

--- src/org/usfirst/frc/team2239/robot/ControlScheme.java
+++ src/org/usfirst/frc/team2239/robot/ControlScheme.java
@@ -20,14 +20,19 @@ public class ControlScheme {
 	private final String LS = "LEFT_SIDE";
 	private final String RS = "RIGHT_SIDE";
 	
+	public HashMap<String, Integer> active;
+	private HashMap<String, Integer> x = new HashMap<String, Integer>();
+	private HashMap<String, Integer> xx = new HashMap<String, Integer>();
+	private HashMap<String, Integer> xj = new HashMap<String, Integer>();
+	private HashMap<String, Integer> j = new HashMap<String, Integer>();
+	private HashMap<String, Integer> jj = new HashMap<String, Integer>();
 	
-	public HashMap<String, Integer> x = new HashMap<String, Integer>();
-	public HashMap<String, Integer> xx = new HashMap<String, Integer>();
-	public HashMap<String, Integer> xj = new HashMap<String, Integer>();
-	public HashMap<String, Integer> j = new HashMap<String, Integer>();
-	public HashMap<String, Integer> jj = new HashMap<String, Integer>();
+	public ControlScheme(String controlScheme) {
+		init();
+		setActive(controlScheme);
+	}
 	
-	public ControlScheme() {
+	public void init(){
 		// buttons
 		x.put(SP, 1);
 		x.put(SH, 2);
@@ -56,43 +61,52 @@ public class ControlScheme {
 		x.put("NONE7", 7);
 		x.put("NONE8", 8);
 		
-//		// buttons
-//		j.put(SP, 1);
-//		j.put(SH, 2);
-//		j.put(LL, 3);
-//		j.put(LR, 4);
-//		j.put(GT, 5);
-//		j.put(GT, 6);
-//		j.put("NONE", 7);
-//		j.put("NONE", 8);
-//		j.put("NONE", 9);
-//		j.put("NONE", 10);// buttons
-//		
-//		// axis
-//		j.put("NOTE", 0);
-//		j.put(LS, 1);
-//		j.put(GS, 2);
-//		j.put(GB, 3);
-//		j.put("NONE", 4);
-//		j.put(RS, 5);
-//		// POV
-//		j.put(TC, 1);
-//		j.put("NONE", 2);
-//		j.put("NONE", 3);
-//		j.put("NONE", 4);
-//		j.put(SS, 5);
-//		j.put("NONE", 6);
-//		j.put("NONE", 7);
-//		j.put("NONE", 8);
+		// buttons
+		j.put(GB, 1);
+		j.put(TCJ, 2);
+		j.put(GT, 3);
+		j.put(LR, 4);
+		j.put(GS, 5);
+		j.put(GT, 6);
+		j.put("NONE", 7);
+		j.put("NONE", 8);
+		j.put("NONE", 9);
+		j.put("NONE", 10);
+		j.put("NONE", 11);
+		j.put("NONE", 12);
+
+		// axis
+		j.put(, 0);
+		j.put(, 1);
+		j.put(, 2);
+		j.put(, 3);
+
+		// POV
+		j.put(TC, 1);
+		j.put("NONE", 2);
+		j.put("NONE", 3);
+		j.put("NONE", 4);
+		j.put(SS, 5);
+		j.put("NONE", 6);
+		j.put("NONE", 7);
+		j.put("NONE", 8);	
 	}
 	
-	
-	
-	
-	
-	
-	
-	
+	public void setActive(String controlScheme){
+		if(controlScheme == "x"){
+			active = x;
+		}else if(controlScheme == "xx"){
+			
+		}else if(controlScheme == "xj"){
+			
+		}else if(controlScheme == "j"){
+			
+		}else if(controlScheme == "jj"){
+			
+		}else{
+			System.out.println("@setActive controlScheme formatted wrong");
+		}
+	}
 	
 	
 }
