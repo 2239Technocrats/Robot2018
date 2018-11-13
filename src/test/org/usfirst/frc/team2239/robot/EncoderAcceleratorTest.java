106bae57ceee74a67e502562692c34d51c71a322
Henry Scheible <henry.scheible@gmail.com>
2018-11-13 17:55:24 -0600

    start writing encoderacceleratortest.java using the junit framework

--- src/test/org/usfirst/frc/team2239/robot/EncoderAcceleratorTest.java
+++ src/test/org/usfirst/frc/team2239/robot/EncoderAcceleratorTest.java
@@ -1,6 +1,25 @@
 package test.org.usfirst.frc.team2239.robot;
 
+import main.org.usfirst.frc.team2239.robot.robot.EncoderAccelerator;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 
 import edu.wpi.first.wpilibj.SpeedControllerGroup;
+
+public class EncoderAcceleratorTest{
+	TechnoDrive driveTrain;
+	WPI_TalonSRX[]  motorsToLookAt;
+	double distance;
+	double maxVelocity;
+	
+	@beforeclass
+	private void setup() {
+
+	}
+
+	@before
+	private void newTestEnvironment() {
+		EncoderAccelerator testEA = new EncoderAccelerator()
+	}
+
+}
