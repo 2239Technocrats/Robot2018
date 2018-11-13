ef70bafdb4d99f26c2742e43ea56ad391237b449
Hopkins Technocrats <frcteam2239@gmail.com>
2018-11-13 16:46:49 -0600

    latest monstrosity, a char[20][2] used to store the type of trigger it uses (ie. button, axis, or POV)

--- src/main/org/usfirst/frc/team2239/robot/ControlScheme.java
+++ src/main/org/usfirst/frc/team2239/robot/ControlScheme.java
@@ -4,28 +4,31 @@ import java.util.*;
 import edu.wpi.first.wpilibj.drive.*;
 
 public class ControlScheme {
-	public char[] triggerTypes = new char[18];
-	private final String SP = "SPEED";
-	private final String SH = "SHIFT";
-	private final String GT = "GRABBER_TOGGLE";
-	private final String LR = "LIFT_RAISE";
-	private final String LL = "LIFT_LOWER";
-	private final String GS = "GRABBER_SUCC";
-	private final String GB = "GRABBER_BLOW";
-	private final String SS = "SPREADSHEET_SAVE";
-	private final String TCX = "TOGGLE_CONTROLLER";
-	private final String TCXX = "TOGGLE_CONTROLLER";
-	private final String TCXJ = "TOGGLE_CONTROLLER";
-	private final String TCJ = "TOGGLE_CONTROLLER";
-	private final String TCJJ = "TOGGLE_CONTROLLER";
-	private final String UPDATE = "UPDATE_ACTIVE_CONTROLLERS";
+	public char[][] triggerTypes = new char[19][2];
+	private final String SP = "SPEED";							//a
+	private final String SH = "SHIFT";							//b
+	private final String GT = "GRABBER_TOGGLE";					//c
+	private final String LR = "LIFT_RAISE";						//d
+	private final String LL = "LIFT_LOWER";						//e
+	private final String GS = "GRABBER_SUCC";					//f
+	private final String GB = "GRABBER_BLOW";					//g
+	private final String SS = "SPREADSHEET_SAVE";				//h
+	private final String TCX = "TOGGLE_CONTROLLER";				//i
+	private final String TCXX = "TOGGLE_CONTROLLER";			//j
+	private final String TCXJ = "TOGGLE_CONTROLLER";			//k
+	private final String TCJ = "TOGGLE_CONTROLLER";				//l
+	private final String TCJJ = "TOGGLE_CONTROLLER";			//m
+	private final String UPDATE = "UPDATE_ACTIVE_CONTROLLERS";	//n
 	
 	//Tank Drive
-	private final String LS = "LEFT_SIDE";
-	private final String RS = "RIGHT_SIDE";
+	private final String LS = "LEFT_SIDE";						//o
+	private final String RS = "RIGHT_SIDE";						//p
 	//Arcade Drive
-	private final String FW = "FORWARDS";
-	private final String ROT = "ROTATION";
+	private final String FW = "FORWARDS";						//q
+	private final String ROT = "ROTATION";						//r
+	
+	private final String NONE = "NONE";							//s  (placeholder)
+	private final String NOTHING = "NONE";						//t  (placeholder)
 	
 	
 	public HashMap<String, Integer> active;
@@ -41,31 +44,79 @@ public class ControlScheme {
 			active = new HashMap<String, Integer>();
 			// buttons
 			active.put(SP, 1);
+			triggerTypes[0][0] = 'a';
+			triggerTypes[0][1] = 'b';
 			active.put(SH, 2);
+			triggerTypes[0][0] = 'b';
+			triggerTypes[0][1] = 'b';
 			active.put(LL, 3);
+			triggerTypes[0][0] = 'e';
+			triggerTypes[0][1] = 'b';
 			active.put(LR, 4);
+			triggerTypes[0][0] = 'd';
+			triggerTypes[0][1] = 'b';
 			active.put(GT, 5);
+			triggerTypes[0][0] = 'c';
+			triggerTypes[0][1] = 'b';
 			active.put(GT, 6);
+			triggerTypes[0][0] = 'c';
+			triggerTypes[0][1] = 'b';
 			active.put(UPDATE, 7);
+			triggerTypes[0][0] = 'n';
+			triggerTypes[0][1] = 'b';
 			active.put(UPDATE, 8);
+			triggerTypes[0][0] = 'n';
+			triggerTypes[0][1] = 'b';
 			active.put("NONE", 9);
+			triggerTypes[0][0] = 's';
+			triggerTypes[0][1] = 'b';
 			active.put("NONE", 10);
+			triggerTypes[0][0] = 's';
+			triggerTypes[0][1] = 'b';
 			// axis
 			active.put("NONE", 0);
+			triggerTypes[0][0] = 's';
+			triggerTypes[0][1] = 'a';
 			active.put(LS, 1);
+			triggerTypes[0][0] = 'o';
+			triggerTypes[0][1] = 'a';
 			active.put(GS, 2);
+			triggerTypes[0][0] = 'f';
+			triggerTypes[0][1] = 'a';
 			active.put(GB, 3);
+			triggerTypes[0][0] = 'g';
+			triggerTypes[0][1] = 'a';
 			active.put("NONE", 4);
+			triggerTypes[0][0] = 's';
+			triggerTypes[0][1] = 'a';
 			active.put(RS, 5);
+			triggerTypes[0][0] = 'p';
+			triggerTypes[0][1] = 'a';
 			// POV
 			active.put(TCX, 1);
+			triggerTypes[0][0] = 'i';
+			triggerTypes[0][1] = 'p';
 			active.put(TCXX, 2);
+			triggerTypes[0][0] = 'j';
+			triggerTypes[0][1] = 'p';
 			active.put(TCXJ, 3);
+			triggerTypes[0][0] = 'k';
+			triggerTypes[0][1] = 'p';
 			active.put(TCJ, 4);
+			triggerTypes[0][0] = 'l';
+			triggerTypes[0][1] = 'p';
 			active.put(TCJJ, 5);
+			triggerTypes[0][0] = 'm';
+			triggerTypes[0][1] = 'p';
 			active.put(SS, 6);
+			triggerTypes[0][0] = 'h';
+			triggerTypes[0][1] = 'p';
 			active.put("NONE", 7);
+			triggerTypes[0][0] = 's';
+			triggerTypes[0][1] = 'p';
 			active.put("NONE", 8);
+			triggerTypes[0][0] = 's';
+			triggerTypes[0][1] = 'p';
 		}
 		
 		else if(name == "j"){
